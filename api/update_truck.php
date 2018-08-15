4683a313ca2c7cbe2f20e1f10716a1426750efa1
Chris-Bitler <crb2547@rit.edu>
2018-08-15 15:11:57 -0400

    Debugging..

--- api/update_truck.php
+++ api/update_truck.php
@@ -41,13 +41,13 @@ $truckId = $results[0]['truck_id'];
 
 file_put_contents('/tmp/test2', print_r($results, true));
 
-$insertResult = $database->update('INSERT INTO `truck_locations` (`truck_id`, `lat`, `long`, `date`) VALUES (:truckId, :lat, :lon, :date)',
+$insertResult = $database->update('INSERT INTO `truck_locations` (`truck_id`, `lat`, `long`, `date`) VALUES (:truckId, :lat, :lon, :datetime)',
     [
         'truckId' => $truckId,
         'lat' => $lat,
         'lon' => $lon,
-        'date' => $truckData['published_at']
+        'datetime' => $truckData['published_at']
     ]
 );
 
-echo json_encode(['result' => $insertResult]);
+echo json_encode(['result' => print_r($insertResult,true)]);
