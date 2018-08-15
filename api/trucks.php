f8676b577338c9264af23abc9fe1064fe3eaefb6
Chris-Bitler <crb2547@rit.edu>
2018-08-15 13:28:59 -0400

    Get trucks from database

--- api/trucks.php
+++ api/trucks.php
@@ -1,9 +1,24 @@
 <?php
+include '../lib/Database.php';
+include '../lib/Secrets.php';
 
-echo json_encode([[
-    'name' => "ASDF",
-    'description' => "ASDF",
-    'twitter' => "@_tweedge",
-    'lat' => 1,
-    'lon' => 1
-]]);
+$databaseCredentials = (new Secrets())->readSecrets();
+$database = new Database(
+    $databaseCredentials['db_user'],
+    $databaseCredentials['db_pass'],
+    $databaseCredentials['db_host'],
+    $databaseCredentials['db_database']
+);
+
+$results = $database->query('SELECT * FROM `truck_locations_memory` AS tlm LEFT JOIN `truck_information` AS ti ON ti.truck_id=tlm.truck_id;');
+$trucks = [];
+
+foreach ($results as $truck) {
+    $trucks[] = [
+        'name' => $truck['name'],
+        'description' => $truck['description'],
+        'twitter' => $truck['twitter'],
+        'lat' => $truck['lat'],
+        'long' => $truck['long']
+    ];
+}
