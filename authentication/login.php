fccc31926eabc8209bcde2b0e1a1da61150b61b0
Chris-Bitler <crb2547@rit.edu>
2018-08-15 16:53:40 -0400

    Basic login functionality

---
+++ authentication/login.php
@@ -0,0 +1,44 @@
+<?php
+include '../lib/Database.php';
+
+session_start();
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Verify we have expected parameters
+if (!$username || !$password) {
+    die("Invalid parameters");
+}
+
+// Connect to database
+$databaseCredentials = (new Secrets())->readSecrets();
+$database = new Database(
+    $databaseCredentials['db_user'],
+    $databaseCredentials['db_pass'],
+    $databaseCredentials['db_host'],
+    $databaseCredentials['db_database']
+);
+
+// Hash the password we were given
+$password = password_hash($password, PASSWORD_BCRYPT, ['cost' => 13]);
+
+// Get the user's hashed password from the database
+$results = $database->query(
+    'SELECT `password` FROM `users` WHERE `username` = :username',
+    [':username' => $username]
+);
+
+// Exit early if the user doesn't exist
+if (count($results) === 0) {
+    die(json_encode(['result' => 'false']));
+}
+
+// Grab the hashed password from the query results
+$dbPassword = $results[0]['password'];
+
+// If the passwords match, set the session
+if ($dbPassword === $password) {
+    $_SESSION['name'] = $username;
+    echo json_encode(['result' => true]);
+}
